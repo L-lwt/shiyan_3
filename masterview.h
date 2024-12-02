d8ff570f004e8edae4161189c259d7ca04f791a6
linwentao <1729963503@qq.com>
2024-12-02 17:04:01 +0800

    first

---
+++ masterview.h
@@ -0,0 +1,50 @@
+#ifndef MASTERVIEW_H
+#define MASTERVIEW_H
+
+#include <QWidget>
+#include "loginview.h"
+#include "doctorview.h"
+#include "departmentview.h"
+#include "patienteditview.h"
+#include "patientview.h"
+#include "welcomeview.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MasterView;
+}
+QT_END_NAMESPACE
+
+class MasterView : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MasterView(QWidget *parent = nullptr);
+    ~MasterView();
+
+public slots:
+    void goLoginView();
+    void goWelcomeView();
+    void goDoctorView();
+    void goDepartmentView();
+    void goPatientEditView(int rowNo);
+    void goPatientView();
+    void goPreviousView();
+
+private slots:
+    void on_btBack_clicked();
+
+private:
+    Ui::MasterView *ui;
+    void pushWidgetToStackView(QWidget *widget);
+
+    WelcomeView *welcomeView;
+    DoctorView *doctorView;
+    PatientView *patientView;
+    DepartmentView *departmentView;
+    LoginView *loginView;
+    PatientEditView *patientEditView;
+
+};
+#endif // MASTERVIEW_H
